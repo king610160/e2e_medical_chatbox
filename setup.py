38cbfd5164b626ae9c83859cbd8877620edec40d
king610160 <king610160@gmail.com>
2024-04-28 20:22:29 +0800

    finish setup.py, also run it to create the Medical_Chatbot folder.

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py is for set up src for the modules
+
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    packages= find_packages(),
+    install_requires = []
+)
